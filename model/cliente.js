a242935e486569a5f0014a92c7b2c8a22a575924
zarkus <lucasferlima8@gmail.com>
2021-12-24 01:04:26 -0300

    Arquitetura MVC implantada com sucesso

---
+++ model/cliente.js
@@ -0,0 +1,11 @@
+class Cliente {
+    constructor(nomeCompleto, nascimento, email, telefone, atividade) {
+        this.nomeCompleto = nomeCompleto;
+        this.nascimento = nascimento;
+        this.email = email;
+        this.telefone = telefone;
+        this.atividade = atividade;
+    }
+}
+
+module.exports = Cliente;
